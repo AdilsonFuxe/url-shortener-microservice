918efbdc8d9d82bed2d79ab7e9965b3a5193964d
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-09-06 23:24:47 +0100

    feat: add Click composite

--- src/interface/controllers/redirect-short-url-controller.ts
+++ src/interface/controllers/redirect-short-url-controller.ts
@@ -1,10 +1,13 @@
-import { LoadUrlByKey } from '../../domain/usecases';
+import { AddClick, LoadUrlByKey } from '../../domain/usecases';
 import { InvalidUrlError } from '../errors';
 import { badRequest, ok, serverError } from '../helpers/http/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../protocols';
 
 export class RedirectShortUrlController implements Controller {
-  constructor(private readonly loadUrlByKey: LoadUrlByKey) {}
+  constructor(
+    private readonly loadUrlByKey: LoadUrlByKey,
+    private readonly addClicks: AddClick
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -13,6 +16,7 @@ export class RedirectShortUrlController implements Controller {
       if (!result) {
         return badRequest(new InvalidUrlError());
       }
+      this.addClicks.addClick(url);
       return ok({ url: result.originalUrl });
     } catch (error) {
       return serverError(error as any);
--- src/main/factories/controller/redirect-short-url-controller.ts
+++ src/main/factories/controller/redirect-short-url-controller.ts
@@ -1,3 +1,4 @@
+import { DbAddClick } from '../../../data/usecases/db-add-click';
 import { DbLoadUrlByKey } from '../../../data/usecases/db-load-url-by-key';
 import { ShortUrlMongooseRepository } from '../../../infra/db/mongoose/repositories/short-url-mongoose-repository';
 import { RedirectShortUrlController } from '../../../interface/controllers';
@@ -6,6 +7,7 @@ import { Controller } from '../../../interface/protocols';
 export const makeRedirectShortUrlController = (): Controller => {
   const shortUrlMongooseRepository = new ShortUrlMongooseRepository();
   const dbLoadUrlByKey = new DbLoadUrlByKey(shortUrlMongooseRepository);
-  const controller = new RedirectShortUrlController(dbLoadUrlByKey);
+  const dbAddClick = new DbAddClick(shortUrlMongooseRepository);
+  const controller = new RedirectShortUrlController(dbLoadUrlByKey, dbAddClick);
   return controller;
 };
