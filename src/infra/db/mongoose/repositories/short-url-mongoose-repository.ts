72ce745b677183f74b42dc8a26af8bc701f320eb
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-09-06 22:05:03 +0100

    feat: add LoadShortUrlByKey Infra

--- src/infra/db/mongoose/repositories/short-url-mongoose-repository.ts
+++ src/infra/db/mongoose/repositories/short-url-mongoose-repository.ts
@@ -1,8 +1,13 @@
-import { AddShortUrlRepository } from '../../../../data/protocols';
+import {
+  AddShortUrlRepository,
+  LoadUrlByKeyRepository,
+} from '../../../../data/protocols';
 import { MongoHelper } from '../helper';
 import { ShortUrlMongooseModel } from '../models';
 
-export class ShortUrlMongooseRepository implements AddShortUrlRepository {
+export class ShortUrlMongooseRepository
+  implements AddShortUrlRepository, LoadUrlByKeyRepository
+{
   async add({
     key,
     url,
@@ -11,5 +16,10 @@ export class ShortUrlMongooseRepository implements AddShortUrlRepository {
     await doc.save();
     const parsedDoc = JSON.parse(JSON.stringify(doc));
     return MongoHelper.serialize(parsedDoc);
+  }
+
+  async loadByKey(key: string): Promise<LoadUrlByKeyRepository.Response> {
+    const url = await ShortUrlMongooseModel.findOne({ shortUrl: key }).lean();
+    return MongoHelper.serialize(url);
   }
 }
